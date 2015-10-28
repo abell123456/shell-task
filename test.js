187db3ba66426fa32a4af226b705d419b9c1ed08
abell123456 <742766812@qq.com>
2015-10-28 20:08:26 +0800

    test.js init

---
+++ test.js
@@ -0,0 +1,17 @@
+var Task = require('./index');
+
+new Task('echo "lets go"')
+    .set({
+        noInterrupt: false
+    })
+    .exec('wrong cmd')
+    .sleep('www')
+    .exec('echo haha')
+    .sleep(5000)
+    .exec('echo \'it went thru!\'')
+    .run(function (err, next) {
+        if (err && next) {
+            console.log('test log:', err);
+            next();
+        }
+    });
